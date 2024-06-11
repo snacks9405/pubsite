3107b45e48f8af04a72b80ae3c4910e718478713
snacks9405 <rodriguezal.525@gmail.com>
2024-06-11 05:39:44 -0500

    edit projectpage

--- src/pages/ProjectPage.jsx
+++ src/pages/ProjectPage.jsx
@@ -86,35 +86,21 @@ const ProjectPage = ({}) => {
 
                 <p className="my-2 bg-indigo-100 p-2 font-bold text-2 ">
                   <Link className="hover:text-3 cursor-pointer" to="https://github.com/CloseRange" target="_blank">
-                    Michael Hullbert
+                    Michael Hulbert
                   </Link>
                   <br />
                   <Link className="hover:text-3 cursor-pointer" to="https://github.com/CloseRange" target="_blank">
-                    Michael Hullbert
+                    Michael Hulbert
                   </Link>
                 </p>
 
-                {/* <h3 className="text-xl">Contact Phone:</h3>
+                {/* <h3 className="text-xl">another box</h3>
 
                 <p className="my-2 bg-indigo-100 p-2 font-bold">
                   get rid of this? 
                 </p> */}
               </div>
 
-              {/* <div className="bg-white p-6 rounded-lg shadow-md mt-6">
-                <h3 className="text-xl font-bold mb-6">Manage Job</h3>
-                <Link
-                  to={`/projects/${project.id}`}
-                  className="bg-indigo-500 hover:bg-indigo-600 text-white text-center font-bold py-2 px-4 rounded-full w-full focus:outline-none focus:shadow-outline mt-4 block"
-                >Edit Job
-                </Link>
-                <button
-                  
-                  className="bg-red-500 hover:bg-red-600 text-white font-bold py-2 px-4 rounded-full w-full focus:outline-none focus:shadow-outline mt-4 block"
-                >
-                  Delete Job
-                </button>
-              </div> */}
             </aside>
           </div>
         </div>
