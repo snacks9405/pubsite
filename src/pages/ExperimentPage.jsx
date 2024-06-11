602ede55760b45696659b1586c1175a5fede114c
snacks9405 <rodriguezal.525@gmail.com>
2024-06-11 06:06:30 -0500

    update experiment page coloring

--- src/pages/ExperimentPage.jsx
+++ src/pages/ExperimentPage.jsx
@@ -3,10 +3,10 @@ import { IoIosConstruct } from "react-icons/io";
 
 const ExperimentPage = () => {
   return (
-    <div className="content-center text-center justify-center text-8xl py-48">
+    <div className="content-center text-center justify-center text-8xl text-2 py-48 ">
       Under Construction
       <br />
-      <IoIosConstruct className="inline items-center"/>
+      <IoIosConstruct className="inline items-center text-3"/>
       <br />
       <text className="text-6xl">
         something cool, coming soon!
